08fd90930fdd095db6eadcdbfd5d7155d327cad5
JuliusAlphonso <JadeMaveric@users.noreply.github.com>
2020-06-03 10:31:11 +0530

    Create counter button

--- Day2/Counter/app/src/main/java/me/juliusalphonso/counter/MainActivity.kt
+++ Day2/Counter/app/src/main/java/me/juliusalphonso/counter/MainActivity.kt
@@ -1,12 +1,26 @@
 package me.juliusalphonso.counter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun incrementCounter(view: View) {
+        val textView = findViewById<TextView>(R.id.display)
+        val message = textView.text.toString().toInt() + 1
+        textView.text = message.toString()
+//        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+//            putExtra(EXTRA_MESSAGE, message)
+//        }
+//        startActivity(intent)
     }
 }
